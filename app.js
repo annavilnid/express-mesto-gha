2610d839f3f99247d56432ea8adf1373fdacc0e2
Anna Vilnid <anna.vilnid@gmail.com>
2022-07-13 21:56:16 +0200

    test 5

--- app.js
+++ app.js
@@ -16,10 +16,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-  next();
+app.all('*', (req, res, next) => {
+  next(res.status(404).send({ message: 'Запрашиваемый пользователь не найден' }));
 });
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
