a318be6dfc3a6fd95d98140dd19071babd09e1d6
Anna Vilnid <anna.vilnid@gmail.com>
2022-07-30 17:17:07 +0200

    404 like dislike

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,6 +1,6 @@
 const Card = require('../models/card');
 const { ERROR_CODE, SERVER_ERROR_CODE } = require('../errors/errors');
-const { NotFoundError } = require('../errors/NotFoundError');
+const NotFoundError = require('../errors/NotFoundError');
 
 module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
@@ -19,12 +19,12 @@ module.exports.getCards = (req, res, next) => {
     .catch((err) => next(err));
 };
 
-module.exports.deleteCard = (req, res) => {
+module.exports.deleteCard = (req, res, next) => {
   Card.findByIdAndRemove(req.params.cardId)
     // eslint-disable-next-line consistent-return
     .then((card) => {
       if (!card) {
-        return new NotFoundError('Запрашиваемая карточка не найдена');
+        next(new NotFoundError('Запрашиваемая карточка не найдена'));
       }
       res.send({ card });
     })
@@ -37,12 +37,12 @@ module.exports.deleteCard = (req, res) => {
     });
 };
 
-module.exports.likeCard = (req, res) => {
+module.exports.likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     // eslint-disable-next-line consistent-return
     .then((card) => {
       if (!card) {
-        return new NotFoundError('Запрашиваемая карточка не найдена');
+        next(new NotFoundError('Запрашиваемая карточка не найдена'));
       }
       res.send({ card });
     })
@@ -55,12 +55,12 @@ module.exports.likeCard = (req, res) => {
     });
 };
 
-module.exports.dislikeCard = (req, res) => {
+module.exports.dislikeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     // eslint-disable-next-line consistent-return
     .then((card) => {
       if (!card) {
-        return new NotFoundError('Запрашиваемая карточка не найдена');
+        next(new NotFoundError('Запрашиваемая карточка не найдена'));
       }
       res.send({ card });
     })
