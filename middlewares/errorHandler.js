ea7f1b07f84bd4dae337ddfa369a23bb3995d8b5
Anna Vilnid <anna.vilnid@gmail.com>
2022-07-31 16:52:43 +0200

    middlewares

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,14 +1,16 @@
+const { NOT_FOUND_CODE } = require('../errors/errors');
+
 const errorHandler = (err, req, res, next) => {
   if (err.name === 'BadRequestError') {
-    res.status(400).send({ message: err.message });
+    res.send({ message: err.message });
   } else if (err.name === 'DuplicateDataError') {
-    res.status(409).send({ message: err.message });
+    res.send({ message: err.message });
   } else if (err.name === 'NotFoundError') {
-    res.status(404).send({ message: err.message });
+    res.send({ message: err.message });
   } else if (err.name === 'ForbiddenError') {
-    res.status(403).send({ message: err.message });
+    res.send({ message: err.message });
   } else {
-    res.status(500).send({ message: 'Ошибка Сервера' });
+    res.status(NOT_FOUND_CODE).send({ message: 'Ошибка Сервера' });
   }
   next();
 };
