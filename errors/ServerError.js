2254d3b8d26f5f259bf4257830d24f44676205d4
Anna Vilnid <anna.vilnid@gmail.com>
2022-08-02 08:58:01 +0200

    users error32

--- errors/ServerError.js
+++
@@ -1,9 +0,0 @@
-// class ServerError extends Error {
-//  constructor(message) {
-//    super(message);
-//    this.name = 'Server error';
-//    this.statusCode = 500;
-//  }
-// }
-
-// module.exports = ServerError;
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   if (err.name) {
-    res.err.status.send({ message: err.message });
+    res.status(err.code).send({ message: err.message });
   } else {
     res.status(500).send({ message: 'Ошибка Сервера' });
   }
