2704e6d0527797a1a749450c109118d97beafeaf
Anna Vilnid <anna.vilnid@gmail.com>
2022-08-08 10:37:12 +0200

    log

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const { auth } = require('./middlewares/auth');
 const { errorHandler } = require('./middlewares/errorHandler');
 const { validateCreateUser, validateLogin } = require('./middlewares/validator');
 const NotFoundError = require('./errors/NotFoundError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 mongoose.connect('mongodb://127.0.0.1/mestodb', {
   useNewUrlParser: true,
@@ -21,6 +22,8 @@ const app = express();
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 
+app.use(requestLogger); // подключаем логгер запросов
+
 // роуты которым не нужна авторизация
 app.post('/signup', validateCreateUser, createUser);
 app.post('/signin', validateLogin, login);
@@ -28,6 +31,8 @@ app.post('/signin', validateLogin, login);
 // роуты которым нужна авторизация
 app.use('/users', auth, userRouter);
 app.use('/cards', auth, cardRouter);
+
+app.use(errorLogger); // подключаем логгер ошибок
 
 app.use((req, res, next) => {
   next(new NotFoundError('Запрашиваемая страница или URL не найдены'));
