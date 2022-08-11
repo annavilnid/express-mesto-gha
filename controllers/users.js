c149a00bfd8ee38502d51ea6e8575f6d558251b2
Anna Vilnid <anna.vilnid@gmail.com>
2022-08-11 09:37:55 +0200

    cors10

--- controllers/users.js
+++ controllers/users.js
@@ -45,7 +45,7 @@ module.exports.login = (req, res, next) => {
   User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
-      res.cookie('jwt', token, { sameSite: true, httpOnly: true });
+      res.cookie('jwt', token, { httpOnly: true, sameSite: 'none', secure: true });
       res.send({ token });
     })
     .catch(next);
