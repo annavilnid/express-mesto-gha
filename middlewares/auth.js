b92dae7b6782a2a396aa500cdf9c6b4feab362ce
Anna Vilnid <anna.vilnid@gmail.com>
2022-08-13 17:29:03 +0200

    test3

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-// require('dotenv').config();
+require('dotenv').config();
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const NODE_ENV = 'production';
